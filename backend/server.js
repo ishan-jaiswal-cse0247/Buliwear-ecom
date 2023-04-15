95339b9aa3a9bee890bef72c2235ffb56d5be1bd
Ishan Jaiswal <ashu.aa79@gmail.com>
2023-04-15 10:08:09 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import path from 'path';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import seedRouter from './routes/seedRoutes.js';
+//import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import boughtRouter from './routes/boughtRoutes.js';
@@ -21,11 +22,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api/seed', seedRouter);
+//app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/bought', boughtRouter);
 app.use('/api/wishlist', wishlistRouter);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
